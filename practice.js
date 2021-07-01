0ae357a6c016aa82a73fb06b08e503400f3c5636
Emily Morgenbesser <emilymorgenbesser@Emilys-MacBook-Air.local>
2021-07-01 15:18:03 -0400

    initial commit

---
+++ practice.js
@@ -0,0 +1,38 @@
+// console.log('Start')
+// setTimeout(function(){
+//     console.log('middle')
+// },5000)
+
+// console.log('done')
+
+// function makeBurger(){
+//     let beef= getBeef()
+//     let patty = cookBeef(beef)
+//     let buns = getBuns()
+//     let burger = putBeefBetweenBuns(buns, patty)
+//     return burger    
+// }
+
+// let burger = makeBurger()
+// serve(burger)
+
+
+// for(let i=0;i<100; i++){
+//     console.log(i % 2){
+// //     } 
+   
+// for(let i=1; i<100; i++){
+//     if(i % 2 !==0) {
+//         console.log(i)
+//     }
+// }
+
+const array = [2, 4, 6, 8]
+const sum = 0
+
+function sumNum(num){
+    for(let i=0; i<num.length; i++)
+    console.log(num[i]+ sum)
+}
+
+sumNum(array)
